6d58ac6c6be10b891eba17981d6ba6a04eb3cf22
0nlyssslatts <sid36205@mail.ru>
2024-10-10 10:51:52 +0300

    number thory added

---
+++ theory/numbers.js
@@ -0,0 +1,57 @@
+// const num = 999
+// const float = 9.99
+// const pow = 10e3
+// const big = 1_000_000_000
+// console.log(Number.MAX_SAFE_INTEGER)
+// console.log(Math.pow(2,53) - 1)
+// console.log(Number.MIN_SAFE_INTEGER)
+// console.log(Number.MAX_VALUE)
+// console.log(Number.MIN_VALUE)
+// console.log(Number.POSITIVE_INFINITY)
+// console.log(Number.NEGATIVE_INFINITY)
+// console.log(1/0)
+// console.log(-1/0)
+// console.log(Number.isFinite(1/0))
+// console.log(234 / undefined)
+// const weird = 23 / undefined
+// console.log(Number.isNaN(weird))
+
+// const strInt = '999'
+// const strFloat = '9.99'
+
+// console.log(Number(strInt))
+// console.log(Number(strFloat))
+// console.log(Number.parseInt(strInt))
+// console.log(Number.parseInt(strFloat))
+// console.log(Number.parseFloat(strFloat))
+//console.log(+strInt,+strFloat) // ВАЖНООООООООООООООООООО
+
+// console.log(+(0.2 + 0.3).toFixed(1))
+
+//BigInt
+
+// console.log(BigInt(Number.MAX_SAFE_INTEGER) + 999999999999999n)
+// console.log(5n / 2n)
+// console.log(5 / 2)
+
+// console.log(Math.E)
+// console.log(Math.PI)
+// console.log(Math.sqrt(25))
+// console.log(Math.pow(2, 10))
+// console.log(Math.abs(-100))
+// console.log(Math.max(2, 6, 7, 23, 423 ,525 ,23 ,532 ,234, 999))
+// console.log(Math.min(2, 6, 7, 23, 423 ,525 ,23 ,532 ,234, 999))
+
+// const myNum = 4.99
+// console.log(Math.floor(myNum))
+// console.log(Math.ceil(myNum))
+// console.log(Math.round(myNum))
+// console.log(Math.trunc(myNum))
+// console.log(Math.random())
+
+// function getRandNum(min, max){
+//     return Math.trunc(Math.random() * (max - min + 1) + min)
+// }
+
+// const randNum = getRandNum(100, 1000)
+// console.log(randNum)
